982549bbc6ea656c4d2f63e6d156f6f48ea2143d
Samuel Ademola <io.samuelademola@gmail.com>
2020-12-07 18:58:14 -0600

    first commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Samuel F. Ademola on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
